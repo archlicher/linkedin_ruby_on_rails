2a574f080577f330ad7e0becce4dd21772b7777a
fori <hristofor_todorov@smartcom.bg>
2016-11-18 17:31:22 +0200

    Add relations: - 1:1 - 1:m - n:m - n:m rich

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,5 @@
 class Page < ApplicationRecord
+	belongs_to :subject #, { :optional => true }#, {:foreign_key => 'foo_id'}
+	has_many :sections
+	has_and_belongs_to_many :admin_users #, :join_table => 'pages_admin_users'
 end
