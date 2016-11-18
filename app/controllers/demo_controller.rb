03f58effa44464159e52db2735b72a5cbdabd2c0
fori <hristofor_todorov@smartcom.bg>
2016-11-18 10:35:53 +0200

    Add all files

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,21 @@
+class DemoController < ApplicationController
+
+	layout false
+
+	def index
+	end
+
+	def hello
+		@array = [1, 2, 3, 4, 5]
+		@id = params[:id]
+		@page = params['page']
+	end
+
+	def other_hello
+		redirect_to(:action => 'hello') #redirect_to(:controller => 'demo', :action => 'index')
+	end
+
+	def lynda
+		redirect_to("http://get.bg")
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+	root 'demo#index'
+
+	get 'demo/index'
+	get 'demo/hello'
+	get 'demo/other_hello'
+	get 'demo/lynda'
+
+end
