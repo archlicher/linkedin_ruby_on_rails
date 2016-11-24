c73a3db96a95fd7d7c25ecdbe7baced1c5432009
fori <hristofor_todorov@smartcom.bg>
2016-11-24 17:33:02 +0200

    Added CRUD operations through web for Subject model - SubjectsController - added methods - routes - resources: subject

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,66 @@
+class SubjectsController < ApplicationController
+  def index
+    @subjects = Subject.sorted
+  end
+
+  def show
+    @subject = subject
+  end
+
+  def new
+    @subject = Subject.new({:name => 'Default'})
+  end
+
+  def create
+    # init new object wil vals
+    @subject = Subject.new(subject_params)
+    # save ?
+    if @subject.save
+      # succ
+      flash[:notice] = "Subject #{@subject.name} created successfully!"
+      redirect_to(subjects_path)
+    else
+      # fail
+      flash[:error] = "Something went wrong"
+      render('new')
+    end
+  end
+
+  def edit
+    @subject = subject
+  end
+
+  def update
+    @subject = subject
+    # save ?
+    if @subject.update_attributes(subject_params)
+      # succ
+      flash[:notice] = "Subject #{@subject.name} updated!"
+      redirect_to(subject_path(@subject))
+    else
+      # fail
+      render('edit')
+    end
+  end
+
+  def delete
+    @subject = subject
+  end
+
+  def destroy
+    @subject.destroy
+    flash[:notice] = "Subject #{@subject.name} deleted!"
+    redirect_to(subjects_path)
+  end
+
+  private
+
+  def subject
+    @subject = Subject.find(params[:id])
+  end
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,34 @@ Rails.application.routes.draw do
 
 	root 'demo#index'
 
+	resources :subjects do
+		member do
+			get :delete
+		end
+	end
+	# get 'subjects/index'
+	# get 'subjects/show/:id'
+
+	# get 'subjects/new'
+	# post 'subjects/create'
+
+	# get 'subjects/edit/:id'
+	# patch 'subjects/update/:id'
+
+	# get 'subjects/delete/:id'
+	# delete 'subjects/destroy/:id'
+
+	resources :pages do
+		member do
+			get :delete
+		end
+	end
+	resources :sections do
+		member do
+			get :delete
+		end
+	end
+
 	get 'demo/index'
 	get 'demo/hello'
 	get 'demo/other_hello'
