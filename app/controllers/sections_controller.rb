5cc28e61c374a5ae8579d8760a3621aa3afa8bf3
fori <hristofor_todorov@smartcom.bg>
2016-12-14 10:14:27 +0200

    Finishing the tutorial

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -4,26 +4,28 @@ class SectionsController < ApplicationController
 
   before_action :confirm_logged_in
   before_action :find_section, :only => [:show, :edit, :update, :delete, :destroy]
-  before_action :find_pages, :only => [:new, :create, :edit, :update]
+  before_action :find_page
+  # before_action :find_pages, :only => [:new, :create, :edit, :update]
   before_action :set_section_count, :only => [:new, :create, :edit, :update]
 
   def index
-    @sections = Section.sorted
+    @sections = @page.sections.sorted
   end
 
   def show
   end
 
   def new
-    @section = Section.new
+    @section = Section.new(:page_id => @page.id)
   end
 
   def create
     @section = Section.new(section_params)
+    @section.page = @page
 
     if @section.save
       flash[:notice] = "Section #{@section.name}"
-      redirect_to(sections_path)
+      redirect_to(sections_path(:page_id => @page.id))
     else
       flash[:error] = "Something went wrong"
       render('new')
@@ -36,7 +38,7 @@ class SectionsController < ApplicationController
   def update
     if @section.update_attributes(section_params)
       flash[:notice] = "Section #{@section.name}"
-      redirect_to(section_path(@section))
+      redirect_to(section_path(@section, :page_id => @page.id))
     else
       flash[:error] = "Something went wrong"
       render('edit')
@@ -49,25 +51,29 @@ class SectionsController < ApplicationController
   def destroy
     @section.destroy
     flash[:notice] = "Deleted section #{@section.name}"
-    redirect_to(sections_path)
+    redirect_to(sections_path(:page_id => @page.id))
   end
 
   private
+
+  def find_page
+    @page = Page.find(params[:page_id])
+  end
 
   def find_section
     @section = Section.find(params[:id])
   end
 
   def section_params
-    params.require(:section).permit(:page_id, :name, :position, :visible, :content, :content_type)
+    params.require(:section).permit(:name, :position, :visible, :content, :content_type)
   end
 
-  def find_pages
-    @pages = Page.sorted
-  end
+  # def find_pages
+  #   @pages = Page.sorted
+  # end
 
   def set_section_count
-    @section_count = Section.count
+    @section_count = @page.sections.count
     if params[:action] == 'new' || params[:action] == 'create'
       @section_count += 1
     end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ApplicationRecord
+
+	acts_as_list :scope => :subject
+
 	belongs_to :subject #, { :optional => true }#, {:foreign_key => 'foo_id'}
 	has_many :sections
 	has_and_belongs_to_many :admin_users #, :join_table => 'pages_admin_users'
